c759ea22f25fe1a53e2def47714daa96d51f6842
Paul deGrandis <paul.degrandis@gmail.com>
2013-08-09 15:11:47 -0700

    One should specify the version of scala, just like they specify the version of Clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scalac "0.1.0"
+(defproject lein-scalac "0.1.1-alpha1"
   :eval-in-leiningen true
-  :dependencies [[org.scala-lang/scala-compiler "2.9.1"]
-                 [lancet "1.0.1"]])
+  :dependencies [[lancet "1.0.2"]])
+
